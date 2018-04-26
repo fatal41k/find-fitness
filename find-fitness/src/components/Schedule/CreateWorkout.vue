5f5bfedfa971b9d2ed535b165d2f96e52b3da871
Yaroslav Hromovyi <gromovoy88@gmail.com>
2018-04-26 15:24:12 +0200

    Added a date picker, when you create a workout

--- find-fitness/src/components/Schedule/CreateWorkout.vue
+++ find-fitness/src/components/Schedule/CreateWorkout.vue
@@ -54,12 +54,36 @@
               ></v-text-field>
             </v-flex>
           </v-layout>
+          <v-layout row class="mb-2">
+            <v-flex xs12 sm6 offset-sm3>
+              <h3>Choose a date: </h3>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="mb-2">
+            <v-flex xs12 sm6 offset-sm3>
+              <v-date-picker
+                class="mt-3"
+                v-model="date"
+                min="2016-06-15"
+                max="2018-08-10"
+              ></v-date-picker>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="mb-2">
+            <v-flex xs12 sm6 offset-sm3>
+              <v-time-picker
+                class="mt-3"
+                v-model="time"
+              ></v-time-picker>
+            </v-flex>
+          </v-layout>
           <v-layout row>
             <v-flex xs12 sm6 offset-sm3>
               <v-btn class="primary"
                      type="submit"
                      :disabled="!formIsValid"
-              >Create</v-btn>
+              >Create
+              </v-btn>
             </v-flex>
           </v-layout>
         </form>
@@ -75,13 +99,28 @@
         title: '',
         location: '',
         imageUrl: '',
-        description: ''
+        description: '',
+        date: null,
+        time: new Date()
       }
     },
     computed: {
       formIsValid () {
         return this.title !== '' && this.location !== '' &&
           this.description !== '' && this.imageUrl
+      },
+      submittableDateTime () {
+        let date = new Date(this.date)
+        if (typeof this.time === 'string') {
+          const hours = this.time.match(/^(\d+)/)[1]
+          const minutes = this.time.match(/:(\d+)/)[1]
+          date.setHours(hours)
+          date.setMinutes(minutes)
+        } else {
+          date.setHours(this.time.getHours())
+          date.setMinutes(this.time.getMinutes())
+        }
+        return date
       }
     },
     methods: {
@@ -94,7 +133,7 @@
           location: this.location,
           imageUrl: this.imageUrl,
           description: this.description,
-          date: new Date()
+          date: this.submittableDateTime
         }
         this.$store.dispatch('createTraining', trainingData)
         this.$router.push('/schedules')
